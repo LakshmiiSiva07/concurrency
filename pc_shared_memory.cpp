cdeb8ce5ae9c2e61777e4313e2b16a8d9aa83d08
Lakshmi Arunachalam <lak6@augusta.cs.pdx.edu>
2018-11-12 16:30:50 -0800

    Defining global variables

--- pc_shared_memory.cpp
+++ pc_shared_memory.cpp
@@ -1,5 +1,15 @@
 #include <iostream>
+#include<pthread.h>
+#include<semaphore.h>
+
 using namespace std;
+
+
+#define BUFFER_SIZE 10
+int buffer[BUFFER_SIZE]; //Buffer to hold the values
+
+pthread_mutex_t mutex;  // Shared memory locking mechanism
+sem_t full,empty;
 
 int main()
 {
