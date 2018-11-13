e39f378057796bdbddb22f3967ec2a19ed067237
Lakshmi Arunachalam <lak6@augusta.cs.pdx.edu>
2018-11-12 17:51:35 -0800

    A basic implementation using 100 threads

--- pc_shared_memory.cpp
+++ pc_shared_memory.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include<pthread.h>
 #include <semaphore.h>
+#include <unistd.h>
 
 using namespace std;
 
@@ -12,30 +13,32 @@ int index = 0;
 pthread_mutex_t mutex;  // Shared memory locking mechanism
 sem_t full,empty;
 
+#define NUM_OF_THREADS 100
+
 // Producer function
-void *producer( void * thread_num)
+void *producer(void * arg)
 {
-	int thread_number = *(int *)thread_num;
+
         sem_wait(&full); // Wait if the buffer is full
 	pthread_mutex_lock(&mutex);
         int produced_value = rand()%1000; // Random value produced
         buffer[index++] = produced_value;
-	cout << "Produced by thread" << thread_number << ",\t item" << produced_value;
+	cout << "Producer thread item " << produced_value << "\n";
 	pthread_mutex_unlock(&mutex);
 	sem_post(&empty);
 	pthread_exit(0);
-
+        
 }
 
 //Consumer function
-void *consumer( void * thread_num)
+void *consumer(void * arg)
 {
-	 int thread_number = *(int *)thread_num;
+
 	 sem_wait(&empty);
 	 pthread_mutex_lock(&mutex);
 	 int consumed_value;
 	 consumed_value =  buffer[--index];
-	 cout << "Consumed by thread" << thread_number << ",\t item" << consumed_value;
+	 cout << "Consumed thread item " << consumed_value << "\n";
          pthread_mutex_unlock(&mutex);
          sem_post(&full);
          pthread_exit(0);
@@ -50,4 +53,13 @@ int main()
 	sem_init(&empty,0,0);
 	sem_init(&full,0,BUFFER_SIZE);
 	pthread_mutex_init(&mutex,NULL);
+    
+	pthread_t producer1[NUM_OF_THREADS],consumer1[NUM_OF_THREADS];
+	for ( int k = 0 ; k < NUM_OF_THREADS; k++)
+	{	
+	int choice = rand() % 2;
+        pthread_create(&consumer1[k],NULL,consumer,NULL);
+	pthread_create(&producer1[k],NULL,producer,NULL);
+	}
+	pthread_exit(NULL);
 }
